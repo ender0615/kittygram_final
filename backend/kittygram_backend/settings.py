d42d1d3de1b1f3fe2a0b54c0c3aed888a15e71fc
Ратмир Хашагульгов <ender0615@mail.ru>
2024-06-27 06:49:04 +0300

    fi

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'key')
 
-DEBUG = os.getenv("DEBUG", 'False').lower() != 'false'
+DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('HOSTS',default='').split()
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'seregasvo.zapto.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
